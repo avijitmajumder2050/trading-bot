91cf5e3336c25747a421e42797da5d8ccb565505
avijitmajumder2050 <avijitmajumder.2050@gmail.com>
2026-02-01 11:19:38 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -62,8 +62,8 @@ async def post_init(app):
     app.create_task(insidebar_breakout_tracker())
     app.create_task(opposite_15m_scheduler())
     app.create_task(opposite_15m_breakout_tracker())
-    app.create_task(ec2_launch_scheduler(launch_hour=9, launch_minute=55))
-    app.create_task(terminate_at(target_hour=10, target_minute=30))
+    app.create_task(ec2_launch_scheduler(launch_hour=11, launch_minute=23))
+    app.create_task(terminate_at(target_hour=12, target_minute=30))
 
 # ───────────────────────────────
 # Main
